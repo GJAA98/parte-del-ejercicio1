0a15327e132aa908bdf4cbbb0cf1092eb84e7399
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:46:05 -0400

    Add files via upload
    Confeccionar una clase que permita carga el nombre y la edad de una persona. Mostrar los datos cargados. Imprimir un mensaje si es mayor de edad (edad>=18).

---
+++ Practica_mayor_de_edad/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica_mayor_de_edad
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ Practica_mayor_de_edad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Practica_mayor_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte el nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Inserte el edad:");
+            string valor = Console.ReadLine();
+            
+            Persona persona = new Persona();
+            persona.Nombre = nombre;
+            persona.Edad = int.Parse(valor);
+
+            Console.WriteLine("Datos insertados:");
+            Console.WriteLine("Nombre:" + persona.Nombre);
+            Console.WriteLine("Edad:" + persona.Edad);
+
+            if (persona.Edad >= 18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("No es mayor de edad");                
+            }
+        }
+    }
+}
